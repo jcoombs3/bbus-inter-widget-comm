d88dc6cc08c7c75f6cd2dfedfd7a7a1f79292456
jcoombs3 <coombsfilm@gmail.com>
2021-01-26 10:46:47 -0500

    working deck container

--- apps/my-example-app/src/app/app.module.ts
+++ apps/my-example-app/src/app/app.module.ts
@@ -1,27 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-import { BackbaseCoreModule } from '@backbase/foundation-ang/core';
-import { environment } from '../environments/environment';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
+import { AppComponent } from "./app.component";
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { BackbaseCoreModule } from "@backbase/foundation-ang/core";
+import { environment } from "../environments/environment";
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+
+import { ContainersModule } from "@backbase/universal-ang/containers";
+import { ProductSummaryWidgetAccountSelectorModule } from "@backbase/retail-ang/product-summary";
+import { TransactionsListWidgetModule } from "@backbase/retail-ang/transactions";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     StoreModule.forRoot({}),
     EffectsModule.forRoot([]),
     HttpClientModule,
     BackbaseCoreModule,
-    RouterModule.forRoot([], { initialNavigation: false, useHash: true })
+    RouterModule.forRoot([], { initialNavigation: false, useHash: true }),
+    ContainersModule,
+    ProductSummaryWidgetAccountSelectorModule,
+    TransactionsListWidgetModule
   ],
-  providers: [...environment.mockProviders || []],
+  providers: [...(environment.mockProviders || [])],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- apps/my-example-app/src/main.ts
+++ apps/my-example-app/src/main.ts
@@ -5,17 +5,42 @@ import { registerSingleApp } from '@backbase/foundation-ang/core';
 import { AppModule } from './app/app.module';
 
 import { environment } from './environments/environment';
+
 if (environment.production) {
   enableProdMode();
 }
 
 const start = registerSingleApp((extraProviders: Array<StaticProvider>) =>
-  platformBrowserDynamic(extraProviders).bootstrapModule(AppModule),
+  platformBrowserDynamic(extraProviders).bootstrapModule(AppModule)
 );
 
 if (environment.bootstrap) {
   const { services, pageModel } = environment.bootstrap;
-  start(services).then(app => {
-    app.bootstrap(pageModel, { parentName: '', index: 0 });
-  });
+
+  if (environment.fetchModel) {
+    environment
+      .fetchModel()
+      .then((res: Response) => {
+        if (res.status !== 200) {
+          console.log(
+            'Unexpected error: Unable to retrieve Experience Model. Status Code: ' +
+              res.status
+          );
+          return;
+        }
+
+        res.json().then(function(data: any) {
+          window.BB.startSingleApp(services).then((app: any) =>
+            app.bootstrap(data.children[0])
+          );
+        });
+      })
+      .catch(function(err: any) {
+        console.log('Fetch Error: Unable to retrieve Experience Model', err);
+      });
+  } else {
+    start(services).then(app => {
+      app.bootstrap(pageModel, { parentName: '', index: 0 });
+    });
+  }
 }
